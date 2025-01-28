ceac26c069fef398d2632850ab37a9e46bfc0428
Pia Albiña <piaalbinamura@gmail.com>
2025-01-28 16:06:14 -0300

    Create Filtro 3.sql

---
+++ SPRINT 7 /Filtro 3.sql
@@ -0,0 +1,17 @@
+SELECT 
+    CASE 
+        WHEN cabs.company_name = 'Flash Cab' THEN 'Flash Cab'
+        WHEN cabs.company_name = 'Taxi Affiliation Services' THEN 'Taxi Affiliation Services'
+        ELSE 'Other'
+    END AS company,
+    COUNT(trips.trip_id) AS trips_amount
+FROM 
+    trips
+JOIN 
+    cabs ON trips.cab_id = cabs.cab_id
+WHERE 
+    CAST(trips.start_ts AS date) BETWEEN '2017-11-01' AND '2017-11-07'
+GROUP BY 
+    company
+ORDER BY 
+    trips_amount DESC;
