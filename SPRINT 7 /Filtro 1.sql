691445150ded113ad1fe4480ff577a793684d195
Pia Albiña <piaalbinamura@gmail.com>
2025-01-28 16:09:41 -0300

    Update Filtro 1.sql

--- SPRINT 7 /Filtro 1.sql
+++ SPRINT 7 /Filtro 1.sql
@@ -1,3 +1,6 @@
+# Imprime el campo company_name. Encuentra la cantidad de viajes en taxi para cada compañía de taxis para el 15 y 16 de noviembre de 2017, asigna al campo resultante el nombre trips_amount e imprímelo también. Ordena los resultados por el campo trips_amount en orden descendente.
+
+
 SELECT
     cabs.company_name,
     COUNT(trips.trip_id) AS trips_amount
