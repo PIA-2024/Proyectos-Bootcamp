e908d785c636b0bd8b3cb38fe1874d0e5de26867
Pia Albiña <piaalbinamura@gmail.com>
2025-01-28 16:11:07 -0300

    Update Filtro 3.sql

--- SPRINT 7 /Filtro 3.sql
+++ SPRINT 7 /Filtro 3.sql
@@ -1,3 +1,5 @@
+# Del 1 al 7 de noviembre de 2017, las empresas de taxis más populares fueron Flash Cab y Taxi Affiliation Services. Encuentra el número de viajes de estas dos empresas y asigna a la variable resultante el nombre trips_amount. Junta los viajes de todas las demás empresas en el grupo "Other". Agrupa los datos por nombres de empresas de taxis. Asigna el nombre company al campo con nombres de empresas de taxis. Ordena el resultado en orden descendente por trips_amount.
+
 SELECT 
     CASE 
         WHEN cabs.company_name = 'Flash Cab' THEN 'Flash Cab'
